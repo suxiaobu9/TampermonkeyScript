0a72146f4e278ece694e1919760cc18848ca49e3
Ari <ari@miniasp.com>
2021-06-03 13:03:15 +0800

    version update

--- Duotify EIP WFH - Auto Fill/Duotify_EIP_Auto_Fill.user.js
+++ Duotify EIP WFH - Auto Fill/Duotify_EIP_Auto_Fill.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Duotify_EIP_Auto_Fill
-// @version         1.4
+// @version         1.41
 // @description     自動勾
 // @author          Ari Su
 // @match           https://duotify-eip.azurewebsites.net/staff/work-from-home/create
